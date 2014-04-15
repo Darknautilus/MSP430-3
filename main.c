d04ad555a86cef0e1c97f5df1cf0af8c61f5987b
Darknautilus <aurelienbertron@gmail.com>
2014-04-15 15:50:35 +0200

    Ajoute code questions restantes

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ unsigned int cpt;
 void init_timer()
 {
   __enable_interrupt();
+  
+  P1IE = 3; // enables interrupts on P1.0 and P1.1
+  
   TACCR0 = 10486;
   TACTL = 2<<8 | // SMCLK
           0<<6 | // divided by 1
@@ -18,16 +21,30 @@ void init_timer()
 #pragma vector=TIMERA0_VECTOR
 __interrupt void mon_traitant_interruption_timer(void)
 {
-  
   lcd_display_number(cpt);
   cpt++;
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void mon_traitant_interruption_bouton(void)
+{
+  if(P1IFG & 1) // si on a appuyé sur S1
+  {
+    cpt = 0; // on remet le compteur à 0
+    lcd_display_number(cpt);
+  }
+  else if(P1IFG & 2)// si on a appuyé sur S2
+  {
+    // on start/stop le timer
+    TACTL = TACTL ^ (1<<4);
+  }
+  P1IFG = 0; // on réinitialise les flags pour pas provoquer une autre interruption
 }
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD; // stop watchdog timer
   lcd_init(); // Initialize driver
-  P5DIR |= 0x02 ; // Set P5.1 to output direction
   cpt = 0;
   lcd_display_number(cpt);
   init_timer();
